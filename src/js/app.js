37a61669f8f4eb32beb6c083afa3e88d08af22a8
SILL-BILL <7th5jack@gmail.com>
2019-05-06 12:50:47 +0900

    update

--- src/js/app.js
+++ src/js/app.js
@@ -32,10 +32,21 @@ var MainScene = /** @class */ (function (_super) {
     */
     function MainScene() {
         var _this = _super.call(this, { key: 'MainScene' }) || this;
+        _this.m_GameState = {
+            'Init': 0,
+            'Wait': 1,
+            'Run': 2,
+            'GameClear': 3,
+            'GameOver': 4,
+            'End': 5
+        };
         _this.m_LivesCnt = Config.LIVES;
         _this.m_Score = 0;
         _this.m_BallCnt = 0;
         _this.m_BlockCnt = 0;
+        _this.m_CurrentGameState = _this.m_GameState.Init;
+        _this.m_TimeElapsed = 0;
+        console.log(_this.m_CurrentGameState);
         return _this;
     }
     /**
@@ -53,11 +64,13 @@ var MainScene = /** @class */ (function (_super) {
         @summary 生成処理
     */
     MainScene.prototype.create = function () {
-        this.physics.world.setBoundsCollision(true, true, true, false);
+        var _this = this;
+        this.physics.world.setBoundsCollision(true, true, true, false); //ワールドのコライダーを設定
         this.m_Bg = this.add.image(Config.CANVAS_WIDTH / 2, Config.CANVAS_HEIGHT / 2, 'bg');
         this.m_Header = this.physics.add.staticImage(Config.CANVAS_WIDTH / 2, 60, 'bg-head');
-        this.m_ScoreText = this.add.text(0, 30, 'Score : ' + this.m_Score.toString(), { fontFamily: 'Arial', fontSize: 48, color: '#f5f5f5' });
+        this.m_ScoreText = this.add.text(30, 30, 'Score : ' + this.m_Score.toString(), { fontFamily: 'Arial', fontSize: 48, color: '#f5f5f5' });
         this.m_LivesText = this.add.text(Config.CANVAS_WIDTH / 2, 30, 'Lives : ' + this.m_LivesCnt.toString(), { fontFamily: 'Arial', fontSize: 48, color: '#f5f5f5' });
+        //各ゲームオブジェクトに当たり判定を設定
         this.m_BallGroup = this.physics.add.group();
         this.m_VausGroup = this.physics.add.group({ immovable: true });
         this.m_BlockGroup = this.physics.add.group({ immovable: true });
@@ -66,27 +79,48 @@ var MainScene = /** @class */ (function (_super) {
         this.physics.add.collider(this.m_BallGroup, this.m_BlockGroup, this.onBlockHit, null, this);
         this.physics.add.collider(this.m_BallGroup, this.m_VausGroup, this.onVausHit, null, this);
         this.physics.add.collider(this.m_BallGroup, this.m_EraserGroup, this.onEraserHit, null, this);
+        //ブロック生成
         this.createBlock(6, 10);
+        //バウス(ラケット)生成
         this.m_Vaus = this.m_VausGroup.create(Config.CANVAS_WIDTH / 2, 1100, 'vaus');
-        this.m_Ball = this.m_BallGroup.create(Config.CANVAS_WIDTH / 2, 1000, 'ball').setCollideWorldBounds(true).setBounce(1).setVelocity(-180, -600);
-        this.m_BallCnt++;
+        //ボール生成
+        this.createBall();
+        //イレーサー生成
         this.m_Eraser = this.m_EraserGroup.create(Config.CANVAS_WIDTH / 2, Config.CANVAS_HEIGHT + 25, 'wall', 'horizontal-red');
+        //クリックイベントの設定
+        this.input.on('pointerup', function (pointer) {
+            if (_this.m_CurrentGameState == _this.m_GameState.Wait) {
+                _this.m_CurrentGameState = _this.m_GameState.Run;
+                _this.m_Ball.setVelocity(-180, -600);
+            }
+        }, this);
+        this.m_CurrentGameState = this.m_GameState.Wait;
     };
     /**
         @summary 更新処理
     */
     MainScene.prototype.update = function (_time, _delta) {
-        this.m_Vaus.x = this.game.input.activePointer.x;
-        if (this.m_Vaus.x < 96) {
-            this.m_Vaus.x = 96;
+        if (this.m_CurrentGameState == this.m_GameState.Run || this.m_CurrentGameState == this.m_GameState.Wait) {
+            this.m_Vaus.x = this.game.input.activePointer.x;
+            if (this.m_Vaus.x < 96) {
+                this.m_Vaus.x = 96;
+            }
+            else if (this.m_Vaus.x > Config.CANVAS_WIDTH - 96) {
+                this.m_Vaus.x = Config.CANVAS_WIDTH - 96;
+            }
         }
-        else if (this.m_Vaus.x > Config.CANVAS_WIDTH - 96) {
-            this.m_Vaus.x = Config.CANVAS_WIDTH - 96;
+        if (this.m_CurrentGameState == this.m_GameState.Wait && this.m_BallCnt > 0) {
+            this.m_Ball.x = this.m_Vaus.x;
         }
-        if (this.m_LivesCnt < 0) {
+        if ((this.m_CurrentGameState == this.m_GameState.Run || this.m_CurrentGameState == this.m_GameState.Wait) && this.m_LivesCnt < 0) {
+            this.m_CurrentGameState = this.m_GameState.GameOver;
             console.log('Game Over!');
         }
-        if (this.m_BlockCnt <= 0) {
+        else if (this.m_CurrentGameState == this.m_GameState.GameOver) {
+            //TODO
+        }
+        if (this.m_CurrentGameState == this.m_GameState.Run && this.m_BlockCnt <= 0) {
+            this.m_CurrentGameState = this.m_GameState.GameClear;
             console.log('Game Clear!');
         }
     };
@@ -127,9 +161,11 @@ var MainScene = /** @class */ (function (_super) {
     MainScene.prototype.onEraserHit = function (_ball, _eraser) {
         _ball.destroy();
         this.m_BallCnt--;
-        if (this.m_BallCnt <= 0) {
+        if (this.m_CurrentGameState == this.m_GameState.Run && this.m_BallCnt <= 0) {
+            this.m_CurrentGameState = this.m_GameState.Wait;
+            this.createBall();
             this.m_LivesCnt--;
-            this.m_LivesText.text = 'Lives : ' + this.m_LivesCnt.toString();
+            this.m_LivesText.text = (this.m_LivesCnt >= 0) ? 'Lives : ' + this.m_LivesCnt.toString() : 'Lives : 0';
         }
     };
     /**
@@ -149,6 +185,22 @@ var MainScene = /** @class */ (function (_super) {
         }
         console.log(this.m_BlockCnt);
     };
+    /**
+        @summary ボール生成
+    */
+    MainScene.prototype.createBall = function () {
+        if (this.m_CurrentGameState == this.m_GameState.Init ||
+            this.m_CurrentGameState == this.m_GameState.Wait) {
+            this.m_Ball = this.m_BallGroup.create(Config.CANVAS_WIDTH / 2, 1025, 'ball').setCollideWorldBounds(true).setBounce(1);
+            this.m_BallCnt++;
+        }
+    };
+    /**
+        @summary シーン遷移
+    */
+    MainScene.prototype.translateScene = function (_sceneName) {
+        this.scene.start(_sceneName);
+    };
     return MainScene;
 }(Phaser.Scene));
 var Main = /** @class */ (function () {
@@ -165,7 +217,7 @@ var Main = /** @class */ (function () {
                 "default": 'arcade',
                 arcade: {
                     // gravity: { y:100 },
-                    debug: false
+                    debug: true
                 }
             },
             scene: [MainScene]
--- src/js/app.ts
+++ src/js/app.ts
@@ -17,6 +17,7 @@ class Config {
 class MainScene extends Phaser.Scene {
 
 	private m_Bg:Phaser.GameObjects.Image;
+
 	private m_Header:Phaser.Physics.Arcade.Image;
 
 	private m_LivesText:Phaser.GameObjects.Text;
@@ -38,6 +39,18 @@ class MainScene extends Phaser.Scene {
 	private m_EraserGroup:Phaser.GameObjects.Group;
 	private m_Eraser:Phaser.Physics.Arcade.Image;
 
+	private m_GameState = {
+		'Init'      : 0,
+		'Wait'      : 1,
+		'Run'       : 2,
+		'GameClear' : 3,
+		'GameOver'  : 4,
+		'End'       : 5
+	}
+	private m_CurrentGameState:number;
+
+	private m_TimeElapsed:number;
+
 	/**
 		@summary コンストラクタ
 	*/
@@ -47,6 +60,11 @@ class MainScene extends Phaser.Scene {
 		this.m_Score = 0;
 		this.m_BallCnt = 0;
 		this.m_BlockCnt = 0;
+		this.m_CurrentGameState = this.m_GameState.Init;
+		this.m_TimeElapsed = 0;
+
+console.log(this.m_CurrentGameState);
+
 	}
 	/**
 		@summary プリロード処理
@@ -64,12 +82,12 @@ class MainScene extends Phaser.Scene {
 	*/
 	public create():void{
 
-		this.physics.world.setBoundsCollision(true, true, true, false);
+		this.physics.world.setBoundsCollision(true, true, true, false); //ワールドのコライダーを設定
 
 		this.m_Bg = this.add.image(Config.CANVAS_WIDTH/2,Config.CANVAS_HEIGHT/2,'bg');
 		this.m_Header = this.physics.add.staticImage(Config.CANVAS_WIDTH/2, 60, 'bg-head');
 		this.m_ScoreText = this.add.text(
-			0,30,
+			30,30,
 			'Score : '+this.m_Score.toString(),
 			{fontFamily:'Arial', fontSize:48, color:'#f5f5f5'}
 		);
@@ -79,6 +97,7 @@ class MainScene extends Phaser.Scene {
 			{fontFamily:'Arial', fontSize:48, color:'#f5f5f5'}
 		);
 
+		//各ゲームオブジェクトに当たり判定を設定
 		this.m_BallGroup = this.physics.add.group();
 		this.m_VausGroup = this.physics.add.group({immovable:true});
 		this.m_BlockGroup = this.physics.add.group({immovable:true});
@@ -88,14 +107,28 @@ class MainScene extends Phaser.Scene {
 		this.physics.add.collider(this.m_BallGroup, this.m_VausGroup, this.onVausHit, null, this);
 		this.physics.add.collider(this.m_BallGroup, this.m_EraserGroup, this.onEraserHit, null, this);
 
+		//ブロック生成
 		this.createBlock(6, 10);
 
+		//バウス(ラケット)生成
 		this.m_Vaus = this.m_VausGroup.create(Config.CANVAS_WIDTH / 2, 1100, 'vaus');
 
-		this.m_Ball = this.m_BallGroup.create(Config.CANVAS_WIDTH / 2, 1000, 'ball').setCollideWorldBounds(true).setBounce(1).setVelocity(-180,-600);
-		this.m_BallCnt++;
+		//ボール生成
+		this.createBall();
 
+		//イレーサー生成
 		this.m_Eraser = this.m_EraserGroup.create(Config.CANVAS_WIDTH/2, Config.CANVAS_HEIGHT+25, 'wall', 'horizontal-red');
+
+		//クリックイベントの設定
+		this.input.on('pointerup', (pointer) => {
+			if (this.m_CurrentGameState == this.m_GameState.Wait)
+			{
+				this.m_CurrentGameState = this.m_GameState.Run;
+				this.m_Ball.setVelocity(-180,-600);
+			}
+		}, this);
+
+		this.m_CurrentGameState = this.m_GameState.Wait;
 
 	}
 	/**
@@ -103,23 +136,36 @@ class MainScene extends Phaser.Scene {
 	*/
 	public update(_time,_delta):void{
 
-		this.m_Vaus.x = this.game.input.activePointer.x;
-		if (this.m_Vaus.x < 96)
-		{
-			this.m_Vaus.x = 96;
-		}
-		else if (this.m_Vaus.x > Config.CANVAS_WIDTH - 96)
-		{
-			this.m_Vaus.x = Config.CANVAS_WIDTH - 96;
+		if(this.m_CurrentGameState == this.m_GameState.Run || this.m_CurrentGameState == this.m_GameState.Wait){
+			this.m_Vaus.x = this.game.input.activePointer.x;
+			if (this.m_Vaus.x < 96)
+			{
+				this.m_Vaus.x = 96;
+			}
+			else if (this.m_Vaus.x > Config.CANVAS_WIDTH - 96)
+			{
+				this.m_Vaus.x = Config.CANVAS_WIDTH - 96;
+			}
 		}
 
-		if(this.m_LivesCnt < 0)
+		if(this.m_CurrentGameState == this.m_GameState.Wait && this.m_BallCnt > 0)
 		{
+			this.m_Ball.x = this.m_Vaus.x;
+		}
+
+		if((this.m_CurrentGameState == this.m_GameState.Run || this.m_CurrentGameState == this.m_GameState.Wait) && this.m_LivesCnt < 0)
+		{
+			this.m_CurrentGameState = this.m_GameState.GameOver;
 			console.log('Game Over!');
 		}
-
-		if(this.m_BlockCnt <= 0)
+		else if(this.m_CurrentGameState == this.m_GameState.GameOver)
 		{
+//TODO
+		}
+
+		if(this.m_CurrentGameState == this.m_GameState.Run && this.m_BlockCnt <= 0)
+		{
+			this.m_CurrentGameState = this.m_GameState.GameClear;
 			console.log('Game Clear!');
 		}
 	}
@@ -163,12 +209,17 @@ class MainScene extends Phaser.Scene {
 		@summary イレーサーヒット時
 	*/
 	private onEraserHit(_ball:Phaser.Physics.Arcade.Image, _eraser:Phaser.Physics.Arcade.Image):void {
+
 		_ball.destroy();
 		this.m_BallCnt--;
-		if(this.m_BallCnt <= 0){
+
+		if(this.m_CurrentGameState == this.m_GameState.Run && this.m_BallCnt <= 0){
+			this.m_CurrentGameState = this.m_GameState.Wait;
+			this.createBall();
 			this.m_LivesCnt--;
-			this.m_LivesText.text = 'Lives : '+this.m_LivesCnt.toString();
+			this.m_LivesText.text = (this.m_LivesCnt >= 0) ? 'Lives : '+ this.m_LivesCnt.toString() : 'Lives : 0';
 		}
+
 	}
 	/**
 		@summary ブロック生成
@@ -197,6 +248,23 @@ class MainScene extends Phaser.Scene {
 
 		console.log(this.m_BlockCnt);
 	}
+	/**
+		@summary ボール生成
+	*/
+	private createBall():void{
+		if(this.m_CurrentGameState == this.m_GameState.Init || 
+			this.m_CurrentGameState == this.m_GameState.Wait)
+		{
+			this.m_Ball = this.m_BallGroup.create(Config.CANVAS_WIDTH / 2, 1025, 'ball').setCollideWorldBounds(true).setBounce(1);
+			this.m_BallCnt++;
+		}
+	}
+	/**
+		@summary シーン遷移
+	*/
+	private translateScene(_sceneName:string):void{
+		this.scene.start(_sceneName);
+	}
 }
 
 class Main {
@@ -218,7 +286,7 @@ class Main {
 				default:'arcade',
 				arcade:{
 					// gravity: { y:100 },
-					debug: false
+					debug: true
 				}
 			},
 			scene: [MainScene]
